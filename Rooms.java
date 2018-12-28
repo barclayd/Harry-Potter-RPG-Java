e48d15420c8a49e6064c02bfcbd15c1b56971198
Daniel Barclay <barclaysd@me.com>
2018-12-28 12:14:50 +0000

    Cleaned code

--- Rooms.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by c1673342 on 30/03/2017.
- */
-public class Rooms {
-    public String [] area = {"The First floor girl's lavatory//Moaning Myrtle's Bathroom", "the Main Castle", "Madam Pomfrey's Hospital Ward", "the Collapsed Tunnel", "The Chamber"};
-}
--- Weapons.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by c1673342 on 30/03/2017.
- */
-public class Weapons {
-    public String [] weaponAttack = { "your Wand", "the Sword of Gryffindor", "some Rocks", "a Basilisk tooth"};
-}
